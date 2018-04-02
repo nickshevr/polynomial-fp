fa3dedb4d12773f6f6b1d72ade6c8d71cfbfa9b8
nickshevr <nickshevr@yandex-team.ru>
2018-04-02 18:19:47 +0300

    Change Exeption -> TypeError

--- polynomial.py
+++ polynomial.py
@@ -3,11 +3,11 @@ import common as fp
 class Polynomial:
     def __init__(self, coefficients):
         if not isinstance(coefficients, list):
-            raise Exception("Coeffs should be list")
+            raise TypeError("Coeffs should be list")
         if fp.isListEmpty(coefficients):
-            raise Exception("Empty coeffs")
+            raise TypeError("Empty coeffs")
         if not fp.isListNumerical(coefficients):
-            raise Exception("Coeffs shouild be float or int")
+            raise TypeError("Coeffs shouild be float or int")
         self.coeffs = fp.sliceZeroCoeffs(coefficients)
         self.degree = fp.size(self.coeffs) - 1
 
@@ -17,7 +17,7 @@ class Polynomial:
         if isinstance(other, Polynomial):
             return Polynomial(fp.sumList(self.coeffs, other.coeffs))
         else:
-            raise Exception()
+            raise TypeError()
 
     def __eq__(self, other):
         if fp.isNumerical(other) and self.degree == 0:
@@ -25,7 +25,7 @@ class Polynomial:
         if isinstance(other, Polynomial):
             return self.coeffs == other.coeffs
         else:
-            raise Exception()
+            raise TypeError()
 
     def __mul__(self, other):
         if fp.isNumerical(other):
@@ -33,7 +33,7 @@ class Polynomial:
         if isinstance(other, Polynomial):
             return Polynomial(fp.polyMul(self.coeffs, other.coeffs))
         else:
-            raise Exception()
+            raise TypeError()
 
     def __str__(self):
         return fp.stringifyPolynomialPretty(self.coeffs)
